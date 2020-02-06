74f98fa271f12be000e25a54c258d4587ac61ac2
GESAlkimi <graci_smith@hotmail.com>
2020-02-06 18:05:04 -0500

    user roles added and working and some more styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: {team_member: 0, admin: 1}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :notes
+
+def user_params
+  devise_parameter_sanitizer.permit(:sign_up)  { |u| u.permit(  :email, :password, :password_confirmation, :role ) }
+end
+
 end
