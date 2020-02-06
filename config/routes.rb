4e1d7370a82ad86eac8f098c5af4c8a67a9dea72
GESAlkimi <graci_smith@hotmail.com>
2020-02-05 23:43:59 -0500

    root changed based on authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  	devise_for :users
   	get 'welcome/index'
-
   	resources:notes
 
-  	root 'welcome#index'
+	authenticated :user do
+		root 'notes#index', as: "authenticated_root"
+	end
 
+  	root 'welcome#index'
 end
